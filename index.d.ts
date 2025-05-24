0d7d8e6d9412185aa23d0a06a39e5ae6680a2972
Serge Kireev <sergekireev@Host-001.lan>
2025-05-24 13:33:18 +0200

    Fix exposed typescript types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-export { saveContractFilesToFs } from './src/io/fs/openContractSource'
+export { saveContractFilesToFs, saveContractFilesToFsV2 } from './src/io/fs/openContractSource'
 
-export { explorerApiKeys, networkNames, explorerApiUrls, ApiName } from './src/explorer/networks'
+export { explorerApiKeys, networkNames, explorerApiUrls, ApiName, v2SupportedNetworks, V2ApiUrl } from './src/explorer/networks'
 export { ContractInfo } from './src/explorer/api-types'
-export { fetchFiles } from './src/explorer/fetchFiles'
+export { fetchFiles, fetchFilesV2 } from './src/explorer/fetchFiles'
